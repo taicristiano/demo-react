fb1136e6cf3cf849470897614561ed5240cd9a2f
Tai Cristiano <taductaik56a3@gmail.com>
2019-02-26 15:10:24 +0700

    Init

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <h1>Hello tai nhot</h1>
+        )
+    }
+}
+
+export default Hello
