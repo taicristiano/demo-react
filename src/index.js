c41853038cb4b4876da0e1111ffae2771d4a0953
Tai Cristiano <taductaik56a3@gmail.com>
2019-02-26 16:04:20 +0700

    Rendering element

--- src/index.js
+++ src/index.js
@@ -10,6 +10,18 @@ ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render('Hello world', document.getElementById('root'))
 ReactDOM.render(<Hello />, document.getElementById('root'))
 
+// Rendering element
+function tick() {
+    const renderingElement = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is { new Date().toLocaleTimeString() }.</h2>
+        </div>
+    )
+    ReactDOM.render(renderingElement, document.getElementById('root'))
+}
+
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
