fbd3e2ae49f8f884e0e5fbde3661cf3bc6980c5c
Tai Cristiano <taductaik56a3@gmail.com>
2019-02-28 10:49:46 +0700

    Forms

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Hello from './Hello';
+import Forms from './Forms';
+import FormsMultipleInput from './FormsMultipleInput';
+
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactDOM.render('Hello world', document.getElementById('root'))
 ReactDOM.render(<Hello />, document.getElementById('root'))
+
+ReactDOM.render(<Forms />, document.getElementById('root'))
+ReactDOM.render(<FormsMultipleInput />, document.getElementById('root'))
+
 
 
 // If you want your app to work offline and load faster, you can change
